dbdd6d0e01875f413c6f8ec965029cea0dcabb6a
Callebe Gomes <callebe.gomes@cnh.com>
2025-04-09 08:55:01 -0500

    using vue + vite + tailwindcss

--- src/bootstrap.ts
+++
@@ -1,5 +0,0 @@
-import "./index.scss"
-
-export * from "./forms";
-
-document.body.innerHTML = "<my-forms></my-forms>"
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-// This lazy-load the bootstrap.ts file.
-// it is required to split the stand-alon application form the module exposed by the module federation
-import("./bootstrap");
