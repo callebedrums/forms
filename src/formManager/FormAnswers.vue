e1fd19ce39f08f625092732eb968c3eda13bc571
Callebe Gomes <callebe.gomes@cnh.com>
2025-04-14 14:08:55 -0500

    adding styles to the list of answers

--- src/formManager/FormAnswers.vue
+++ src/formManager/FormAnswers.vue
@@ -21,19 +21,20 @@ watchEffect(() => {
 
 <template>
   <div class="w-full py-4">
-    <table class="w-full">
-      <thead>
+    <h2 class="text-2xl mb-4">{{ form?.name }}</h2>
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+      <thead class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
         <tr>
-          <th></th>
-          <th v-for="(fields, fi) in form.fields" :key="fi">
+          <th scope="col" class="px-6 py-3"></th>
+          <th scope="col" class="px-6 py-3" v-for="(fields, fi) in form.fields" :key="fi">
             {{ fields.label }}
           </th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(answer, index) in answers" :key="answer.id">
-          <td>{{ index+1 }}</td>
-          <td v-for="(fanswer, fai) in answer.answers" :key="fai">
+        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200 hover:bg-gray-200 dark:hover:bg-gray-600" v-for="(answer, index) in answers" :key="answer.id">
+          <td class="px-6 py-4">{{ index+1 }}</td>
+          <td class="px-6 py-4" v-for="(fanswer, fai) in answer.answers" :key="fai">
             {{ fanswer.value }}
           </td>
         </tr>
--- src/formManager/FormManagerView.vue
+++ src/formManager/FormManagerView.vue
@@ -63,12 +63,29 @@ function deleteForm(f: Form) {
 
 </script>
 <template>
-  <nav>
-    <RouterLink :to="{ query: getTabQuery() }">Edit</RouterLink>
-    <RouterLink :to="{ query: getTabQuery('answer') }">Answers</RouterLink>
-  </nav>
   <div>
-    <h2>{{ form?.name }}</h2>
+    <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+      <ul class="flex flex-wrap -mb-px">
+        <li class="me-2">
+          <RouterLink
+            :to="{ query: getTabQuery() }"
+            class="text-gray-500 inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 aria-selected:text-blue-600 aria-selected:border-blue-600 aria-selected:dark:text-blue-500 aria-selected:dark:border-blue-500"
+            :aria-selected="!tab"
+          >
+            Edit
+          </RouterLink>
+        </li>
+        <li class="me-2">
+          <RouterLink
+            :to="{ query: getTabQuery('answer') }"
+            class="text-gray-500 inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 aria-selected:text-blue-600 aria-selected:border-blue-600 aria-selected:dark:text-blue-500 aria-selected:dark:border-blue-500"
+            :aria-selected="tab === 'answer'"
+          >
+            Answers
+          </RouterLink>
+        </li>
+      </ul>
+    </div>
     <div v-if="!tab">
       <FormEditor :form="form" @save="save" @delete="deleteForm(form)" />
     </div>
